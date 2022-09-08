5342564b13648870c23d270e5fe38bebf83d7fd4
dani balo <daniyemak@gmail.com>
2022-09-08 14:30:10 -0700

    Task 6 size ofdata_types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,14 @@ int main(void)
 	int a;
 	long int b;
 	long long int c;
-	char d;
-	float e;
+	char x;
+	float y;
 
-	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(y));
 
 
 
