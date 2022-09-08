1fe9cf69d22c20c871748660cf8f1229408bf151
dani balo <daniyemak@gmail.com>
2022-09-08 14:45:14 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,7 @@ int main(void)
 	printf("Size of a long int:%lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int:%lu byte(s)\n", sizeof(c));
 	printf("Size of a float:%lu byte(s)\n", sizeof(y));
+	return (0);
 
 
 
