a11445618907fc86fd00e9b2746b335a5c1fa106
dani balo <daniyemak@gmail.com>
2022-09-09 13:54:16 -0700

    without variable charsingle digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Prints a numbers since 0 t0 9.
+ * Return: always success
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= '9')
+	{
+		putchar(a);
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
+
