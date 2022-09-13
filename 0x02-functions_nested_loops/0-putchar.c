713a8e60836eb23a9006b80b50af56d241979929
dani balo <daniyemak@gmail.com>
2022-09-13 03:13:24 -0700

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-	char c[] ="_putchar";
-	for (int i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
