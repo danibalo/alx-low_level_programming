66fcdb2cd61a320e7224c08c9f70b4b562dfcc29
dani balo <daniyemak@gmail.com>
2022-09-13 08:35:57 -0700

    cheks wether the letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- *_islower-checks for lowercase character.
  *
- *Return: 0
+ *
+ *
  */
 int _islower(int c)
 {
-	int c;
+
 	if (islower(c))
-
-
 	{
-		printf("%d is lower case \n", 1, islower(c));
+		printf("%d is lower case \n", 1, _islower);
 	}
 	else
 	{
-	       	printf("%d is lower case \n", 0);
+		printf("%d is not\n", 0);
 	}
 }
 
