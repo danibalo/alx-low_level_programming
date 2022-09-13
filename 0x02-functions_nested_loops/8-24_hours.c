8b4f60d8015fd225336e67addf539f9a0193c526
dani balo <daniyemak@gmail.com>
2022-09-13 13:40:52 -0700

    prints minutes and hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+# include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *  starting from 00:00 to 23:59.
+ *  Return: Sum of them
+ */
+
+
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	while (hr < 24)
+	{
+		while (min < 60)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+			min++;
+		}
+		hr++;
+	}
+
