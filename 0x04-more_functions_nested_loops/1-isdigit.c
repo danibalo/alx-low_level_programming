4232c503272895114122b8d69fe20d7e78cd8918
dani balo <daniyemak@gmail.com>
2022-09-15 11:34:34 -0700

    test for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - it checks for a digit through 0 to 9
  * @c: is the character to be tested
- * Returns 1 if it is a digit, else 0
+ * Return: 1 if it is a digit, else 0
  */
 int _isdigit(int c)
 {
