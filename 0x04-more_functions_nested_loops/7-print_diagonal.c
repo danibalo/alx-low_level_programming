9a3a702f551dc64b91e1da21947fb7c3e5b33123
dani balo <daniyemak@gmail.com>
2022-09-16 11:51:09 -0700

    diagonal character

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: number of characters '\'
+ * Return: Always zer
+ */
+void print_diagonal(int n)
+{
+	int r, c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 0; r < n; r++)
+		{
+			for (c = 0; c <= r; c++)
+			{
+				_putchar('\');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
