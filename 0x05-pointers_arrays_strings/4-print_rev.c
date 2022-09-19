df6b31284c0226a2f966cb0572c110c5e256ddf3
dani balo <daniyemak@gmail.com>
2022-09-19 14:42:42 -0700

    Task reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s : string to be printed
+ * Return: Always zero
+ */
+void print_rev(char *s)
+{
+	int i, k;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j++;
+	}
+	for (k = (j-1); k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
+
+}
