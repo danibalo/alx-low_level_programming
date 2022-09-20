fd6596809d0a85d9de9f3dcbf7136b68925c6508
dani balo <daniyemak@gmail.com>
2022-09-20 14:43:40 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - Function that convert strin to int
+ * @arr: String to convert to int
+ *
+ * Return: Value of converted number
  */
-int _atoi(char *s)
+int _atoi(char *arr)
 {
+	int i = 0, convert = 0;
 
-	return (0);
+	for (i = 0; arr[i] != '\0'; ++i)
+	{
+		convert = convert * 10 + arr[i] - '0';
+	}
+
+	return (convert);
 }
