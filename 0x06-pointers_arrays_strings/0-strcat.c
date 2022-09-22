fa8c9b50ae198e1cd4ab3b8f5b7d3de9db32d3c7
dani balo <daniyemak@gmail.com>
2022-09-22 11:06:39 -0700

    concanate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  **_strcat - concanates two strings
  *@dest: the destination string
@@ -11,12 +12,15 @@ char *_strcat(char *dest, char *src)
 	int dest_len = 0;
 
 	while (dest[dest_len])
-		dest_len++;
-	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[dest_len] = src[i];
-		dest_len +=1;
+		dest_len++;
 	}
+	for (i = 0; src[i] != 0; i++)
+		{
+			dest[dest_len] = src[i];
+			dest_len++;
+		}
+	
 	dest[dest_len] = '\0';
 	return (dest);
 }
