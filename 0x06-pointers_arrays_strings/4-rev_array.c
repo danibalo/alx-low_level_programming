913e9587bd4df34842b2c56145d80ea99e0a659e
dani balo <daniyemak@gmail.com>
2022-09-22 12:51:36 -0700

    reversing array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses elements of a given array
+ * @a: the array to be tested
+ * @n - elements of aray
+ * Retturn: Always success
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, k, b;
+	
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+	for (k = 0; k < i/2; k++)
+	{
+		b = a[k];
+		a[k] = *p;
+		*p = b;
+		b--;
+	}
+}
+
