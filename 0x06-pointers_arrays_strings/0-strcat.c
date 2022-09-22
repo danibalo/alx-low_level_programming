68021159abaf8352ed46b885fd991e51d9fc13f2
dani balo <daniyemak@gmail.com>
2022-09-22 09:57:35 -0700

    concanate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+/**
+ **_strcat - concanates two strings
+ *@dest: the destination string
+ * @src: the source string to be moved.
+ * Return: @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+	for (i = 0; src[i]; i++)
+		dest[dest_len] = src[i];
+	return (dest);
+}
+
