5978cfbb7d3f3ce2a602280c25cacba55e5848c5
dani balo <daniyemak@gmail.com>
2022-09-28 05:44:49 -0700

    prints string followed by new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion(char *s) - prints string followed by newline
+ * @s: string to be printed
+ *
+ * Return: 0
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	if s[i] = '\0'
+		return 0;
+	while (s[i] = '\0')
+		i++;
+	_putchar(i);
+	_putchar('\n');
+}
