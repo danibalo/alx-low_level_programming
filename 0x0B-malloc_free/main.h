707bffd081d6f39372f4416ac4c396dce237c95f
dani balo <daniyemak@gmail.com>
2022-10-05 08:39:54 -0700

    a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,3 +7,4 @@ char *str_concat(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
