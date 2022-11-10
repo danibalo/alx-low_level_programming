4722ff30a055f7d75ce7e1f7dc54db370f741578
danibalo <daniyemak@gmail.com>
2022-11-10 07:49:04 -0800

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,47 +6,39 @@
  */
 void print_times_table(int n)
 {
+	int i, j;
+	int mul;
 
-	int i, j, pr;
-
-
-	if (!(n < 15 || n < 0))
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			{ 
-				for (j = 0; j <=n; j++)
-				{
-					pr = i * j;
-					if j != 0;
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					if (pr < 10 && j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((pr % 10) + '0');
-					}
-					else if (res >= 10 && pr < 100)
-					{
-						_putchar(' ');
-						_putchar((pr / 10) + '0');
-						_putchar((pr % 10) + '0');
-					}
-					else if (pr >= 100 && j != 0)
-					{
-						_putchar((pr / 100) + '0');
-						_putchar((pr / 10) % 10 + '0');
-						_putchar((pr % 10) + '0');
-					}
-					else
-						_putchar((pr % 10) + '0');
-				}
-				_putchar('\n');
-	
-			}
-		}						
-}
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar (',');
+				_putchar (' ');
+				mul = i * j;
 
+				if (mul <= 99)
+					_putchar(' ');
+				if (mul <= 9)
+					_putchar(' ');
+				if (mul >= 100)
+				{
+					_putchar(mul / 100 + '0');
+					_putchar((mul / 10) % 10 + '0');
+					_putchar(mul % 10 + '0');
+				}
+				else if (mul >= 10 && mul <= 99)
+				{
+					_putchar(mul / 10 + '0');
+					_putchar(mul % 10 + '0');
+				}
+				else
+					_putchar(mul + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
