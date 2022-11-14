e53b2e99bf02ce8f73df351e5c99571e321972b3
danibalo <daniyemak@gmail.com>
2022-11-14 11:03:50 -0800

    first 98 fibonnaci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main -  prints the first 98 Fibonacci numbers,
+*Return : zero
+*/
+int main(void)
+{
+	int count;
+       unsigned long fib1 = 0, fib2 = 1, sum = 0;
+
+	while (count < 97)
+	{
+		sum = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = sum;
+		count ++;
+		printf("%lu, ", sum);	
+	}
+	printf("\n");
+
+	return (0);
+}
+
