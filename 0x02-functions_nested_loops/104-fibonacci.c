3cb44271ed9122db3e2979cbd839c9e9bcaf6633
danibalo <daniyemak@gmail.com>
2022-11-14 11:08:53 -0800

    fib 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
 *main -  prints the first 98 Fibonacci numbers,
-*Return : zero
+*Return: zero
 */
 int main(void)
 {
 	int count;
-       unsigned long fib1 = 0, fib2 = 1, sum = 0;
+	unsigned long fib1 = 0, fib2 = 1, sum = 0;
 
 	while (count < 97)
 	{
 		sum = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = sum;
-		count ++;
-		printf("%lu, ", sum);	
+		count++;
+		printf("%lu, ", sum);
 	}
 	printf("\n");
 
