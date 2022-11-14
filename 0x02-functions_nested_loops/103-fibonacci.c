e059359f8dfd8b074feb4e2f2e4ac80f83ccd45b
danibalo <daniyemak@gmail.com>
2022-11-14 10:29:16 -0800

    even fibonnaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - prints the sum of the even-valued terms, followed by a new line.
+*Return: Always zero
+*/
+int main(void)
+{
+unsigned int count, fib1 = 1, fib2 = 2, sum = 0; sum2 = 0;
+for (count = 0; count < 49; count++)
+{
+	if ((fib2 % 2 == 0) && (fib2 <= 4000000))
+	{
+		sum2 = sum2 + fib2;
+	}
+	sum = fib1 + fib2;
+	fib1 = fib2;
+	fib2 = fib1;
+}
+printf("%ld\n", sum2);
+return (0);
+}
