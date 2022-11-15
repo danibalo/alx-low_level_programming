dd6ca4b608d7c81ee7e850f7ff62cac88004d29d
danibalo <daniyemak@gmail.com>
2022-11-15 01:42:04 -0800

    10 times 1 - 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
+				_putchar ('0' + j / 10);
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
