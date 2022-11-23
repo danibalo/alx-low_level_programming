797278f5b9f2df13860a1fcc08f1e6335950db1e
danibalo <daniyemak@gmail.com>
2022-11-23 05:54:43 -0800

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,25 +7,18 @@
  */
 void puts_half(char *str)
 {
-	int i, n, len_str = 0, j;
+	int i = 0, n;
 
-	for (i = 0; str[i] != 0; i++)
+	while(str[i] != '\0')
+		i++;
+	n = i / 2;
+
+	if (n % 2 == 1)
+		n = (i + 1) / 2;
+	while (n < i)
 	{
-		len_str++;
-	}
-	if (len_str % 2 == 0)
-	{
-		for (j = len_str / 2; j < len_str; j++)
-		{
-			_putchar(str[j]);
-		}
-	}
-	else if ((len_str % 2) != 0)
-	{
-		for (n = (len_str - 1) / 2; n < len_str - 1; n++)
-		{
-			_putchar(str[n]);
-		}
+		_putchar(str[n]);
+		n++;
 	}
 	_putchar('\n');
 }
