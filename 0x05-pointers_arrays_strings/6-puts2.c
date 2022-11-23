f5a9d3ef83828a1df7852bc4cdc0261578170196
danibalo <daniyemak@gmail.com>
2022-11-23 04:01:35 -0800

    reverse string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue;
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
+
