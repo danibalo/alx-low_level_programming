c3d96abcfd7074994506b467f5af29a2a66b0a1e
danibalo <daniyemak@gmail.com>
2022-11-24 05:16:10 -0800

    ascii to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,14 +15,14 @@ int _atoi(char *s)
 	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
-		sign = -1;
+		sign *= -1;
 		i++;
 	}
 	if (s[i] != '\0')
 	{
 		do
 		{
-			result = result * 10 + s[i] - '0';
+			result = result * 10 + (s[i] - '0');
 			i++;
 		}
 		while (s[i] >= '0' && s[i] <= '9');
