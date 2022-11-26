8c1203a5b34a433bc209f90f5dc5abb2e2c1e5b7
danibalo <daniyemak@gmail.com>
2022-11-26 00:58:31 -0800

    string concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,15 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (*(dest + i) != '\0')
+	while (*(dest + i))
 		i++;
-	while (*(src + j) != '\0')
+	while (*(src + j))
 	{
-		*(dest + i + j) = *(src + j);
+		*(dest + i) = *(src + j);
+		i++;
 		j++;
 	}
-	*(dest + i + j) = '\0';
+	*(dest + i + 1) = '\0';
 	return (dest);
 }
 
