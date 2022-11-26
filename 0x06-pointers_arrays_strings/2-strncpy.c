d8f8fd2ad31119754811fe479b3b9cedad94f9ba
danibalo <daniyemak@gmail.com>
2022-11-26 11:25:11 -0800

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && src[i] != '\0')
-	{
+	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
-			i++;
-	}
 	for (; n > i; i++)
 	dest[i] = '\0';
        return (dest);
 }
-
