f482492b0b00b03bfc8a303ca0f50dfd0766c52c
danibalo <daniyemak@gmail.com>
2022-11-26 21:32:16 -0800

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,40 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0, l = 0, m;
+	int i = 0, j = 0;
 
-	while (s1[i])
+	while (s1[i] && s2[j])
 	{
+		if (s1[i] == s2[j])
+			return (0);
+		else
+			return (s1[i] - s2[j]);
 		i++;
-	}
-	while (s2[j])
-	{
 		j++;
 	}
-	if (i <= j)
-	{
-		m = i;
-	}
-
-	else
-	{
-		m = j;
-	}
-	while (k <= m)
-	{
-		if (s1[k] == s2[k])
-		{
-			k++;
-			continue;
-		}
-		else
-		{
-			l = s1[k] - s2[k];
-				break;
-		}
-		k++;
-	}
-return (l);
+	return (0);
+	
 }
 
 
