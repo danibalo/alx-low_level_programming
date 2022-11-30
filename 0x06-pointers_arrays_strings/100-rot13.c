f4333402e6dc48f208f01310d5c57837bed0c272
danibalo <daniyemak@gmail.com>
2022-11-30 09:24:07 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * rot13 - encodes a string using rot13.
+ * Return: Success
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char A[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char B[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while(s[i])
+	{
+		for (j = 0; A[j]; j++)
+		{
+			if (s[i] == A[j])
+			{
+				s[i] = B[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
