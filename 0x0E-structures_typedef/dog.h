75d2d296bfa38181a11b8e1a6026caed44e86b79
danibalo <daniyemak@gmail.com>
2022-12-06 09:56:05 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 #include <stdio.h>
+/*
+ * dog - struct
+ * @name: attribute name
+ * @age: attribute of age
+ * @owner: attribute owner of the dog 
+ */
 struct dog{
 	char *name;
 	float age;
