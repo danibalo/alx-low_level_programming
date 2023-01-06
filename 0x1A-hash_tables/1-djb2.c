b5671b628667f0282ebd5953aa5a674bff8bcf44
danibalo <daniyemak@gmail.com>
2023-01-06 09:29:33 -0800

    a hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,12 +8,12 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int c;
+	int count;
 
 	hash = 5381;
-	while ((c = *str++))
+	while ((count = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + count; /* hash * 33 + c */
 	}
 	return (hash);
 }
